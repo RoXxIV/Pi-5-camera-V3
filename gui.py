54ac94083534dcc4b4870f74819a1bf69f576385
RoXxIV <evan.hermier@gmail.com>
2024-11-19 19:20:11 +0100

    add slider

--- gui.py
+++ gui.py
@@ -19,9 +19,19 @@ class CameraApp(ctk.CTk):
         self.photo_button = ctk.CTkButton(self, text="Prendre une photo", command=self.camera.take_photo)
         self.photo_button.pack(pady=10)
 
+        # slider
+        self.slider_label = ctk.CTkLabel(self, text="Réglage inutile pour l'instant")
+        self.slider_label.pack(pady=10)
+
+        self.slider = ctk.CTkSlider(self, from_=0, to=100, command=self.slider_event)
+        self.slider.pack(pady=10)
+
         # Bouton arréter
         self.exit_button = ctk.CTkButton(self, text="éteindre", command=self.stop_app)
         self.exit_button.pack(pady=10)
+
+    def slider_event(self, value):
+        print(f"Valeur du slider : {value}")
 
     def stop_app(self):
         # Arréte la cam et ferme l'appli.
